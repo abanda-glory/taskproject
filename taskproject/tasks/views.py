0d6e6b4932428a3ecd595524f3a1dbec3e8ea959
Abanda Glory <abandaglory91@gmail.com>
2026-03-17 21:35:18 +0100

    worked on the login and registration form

--- taskproject/tasks/views.py
+++ taskproject/tasks/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Task
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+class TaskForm(forms.ModelForm):
+    class Meta: #Meta class is used for configurations
+        model = Task
+        fields = ['title', 'description', 'completed'] #form fields
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_update(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST,  instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_delete(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.delete()
+    return redirect('task_list')
+
+#Register User
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tasks/register.html', {'form': form})
+
+#User Login
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = AuthenticationForm
+    return render(request, 'tasks/login.html', {'form': form})
+
+#User logout
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 # Create your views here.
